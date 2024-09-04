5218f49b85d9fc4c166e8b2c3e8566b78a7d9c9e
Milos Savkovic <milos.savkovic.somias@gmail.com>
2024-09-04 10:48:07 +0200

    Exercise 2: Load with loader and display data in UI

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,36 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  const data = {
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  };
+
+  return json(data);
+};
+
 export default function Posts() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {data.posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
