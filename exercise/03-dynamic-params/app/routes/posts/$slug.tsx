1171f4aaf69a92e6f537fb41fb5a440bc4d70c7f
Milos Savkovic <milos.savkovic.somias@gmail.com>
2024-09-04 12:46:42 +0200

    Exercise 3: Display markdown content

--- exercise/03-dynamic-params/app/routes/posts/$slug.tsx
+++ exercise/03-dynamic-params/app/routes/posts/$slug.tsx
@@ -1,6 +1,7 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { marked } from "marked";
 import { getPost } from "~/models/post.server";
 
 export async function loader({ params }: LoaderArgs) {
@@ -13,6 +14,7 @@ export default function PostSlug() {
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: marked(post?.markdown) }} />
     </main>
   );
 }
