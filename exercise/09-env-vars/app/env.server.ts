ddc80834520d51b3590f936a6c9e681ad2e4ba3b
milos.savkovic <milos.savkovic@nites.eu>
2024-09-09 12:06:47 +0200

    Exercise 9: Set types for ENV

--- exercise/09-env-vars/app/env.server.ts
+++ exercise/09-env-vars/app/env.server.ts
@@ -1,5 +1,17 @@
+import invariant from "tiny-invariant";
+
 export function getEnv() {
+  invariant(process.env.ADMIN_EMAIL, "Admin email is not set");
   return {
     ADMIN_EMAIL: process.env.ADMIN_EMAIL,
   };
+}
+
+type ENV = ReturnType<typeof getEnv>;
+
+declare global {
+  var ENV: ENV;
+  interface Window {
+    ENV: ENV;
+  }
 }
